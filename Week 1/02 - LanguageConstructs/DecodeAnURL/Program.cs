6b94b5d66da4617dd0057ba12914c40c0b3317ce
Viktor Barzin <vbarzin@gmail.com>
2015-11-21 01:55:49 +0200

    added decode url problem

---
+++ Week 1/02 - LanguageConstructs/DecodeAnURL/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecodeAnURL
+{
+    class Program
+    {
+        public static string DecodeUrl(string url)
+        {
+            for (int i = 0; i < url.Length; i++)
+            {
+                url = url.Replace("%20", " ");
+                url = url.Replace("%3A", ":");
+                url = url.Replace("%3D", "?");
+                url = url.Replace("%2f", "/");
+            }
+            return url;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter URL to decode :");
+            string inputUrl = Console.ReadLine();
+            Console.WriteLine(DecodeUrl(inputUrl));
+        }
+    }
+}
