8decd2d61376da69032b4e2240e82219441c5bdd
Viktor Barzin <vbarzin@gmail.com>
2015-11-21 13:13:45 +0200

    updated sum of all numbers in a string

--- Week 1/01 - Introduction to C#/SumAllNumbersInAGivenString/Program.cs
+++ Week 1/01 - Introduction to C#/SumAllNumbersInAGivenString/Program.cs
@@ -16,45 +16,33 @@ namespace SumAllNumbersInAGivenString
 {
     class Program
     {
-        public static int ExtractNumbers(string input)
+        public static int NumbersToAdd(string input)
         {
-            string inputAsInt = Regex.Replace(input, "[^0-9 _]", "");
-            return int.Parse(inputAsInt);
-        }
-        public static List<int> NumbersToAdd(string input)
-        {
-            List<int> Numbers = new List<int>();
+            int temp = 0;
+            int sum = 0;
             for (int i = 0; i < input.Length; i++)
             {
                 if (Char.IsDigit(input[i]))
                 {
-
-                    for (int k = i+1; k < input.Length; k++)
-                    {
-                        if (Char.IsDigit(input[k]))
-                        {
-                            //Numbers.Add(input[k]);
-                        }
-                    
-                    }
-                    //Numbers.Add(input[i]);
+                    int digit = input[i] - '0';
+                    temp = temp * 10 + digit;
+                }
+                else
+                {
+                    sum += temp;
+                    temp = 0;
                 }
             }
-
-            return Numbers;
+            return sum;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter string :");
             string input = Console.ReadLine();
 
-            Console.WriteLine(ExtractNumbers(input));
+            Console.WriteLine(NumbersToAdd(input));
+            //TODO: works if input ends with char
 
-            foreach (var item in NumbersToAdd(input))
-            {
-                Console.WriteLine(item);
-            }
-            //TODO: finish later
         }
     }
 }
