bcde728fcb0133c7ad71e1a4ea7099393ffe38e1
Viktor Barzin <vbarzin@gmail.com>
2015-11-26 22:03:17 +0200

    week 2 day 2

---
+++ Week 2/MethodsPartTwo/DateSums/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateSums
+{
+    class Program
+    {
+        public static int digitsSum(int number)
+        {
+            List<int> currDayDigits = new List<int>();
+            int sum = 0;
+            while (number > 0)
+            {
+                currDayDigits.Add(number % 10);
+                number = number / 10;
+            }
+            for (int i = 0; i < currDayDigits.Count; i++)
+            {
+                sum += (int)currDayDigits[i];
+            }
+            return sum;
+        }
+        public static void PrintDatesWithGivenSum(int year, int sum)
+        {
+            DateTime currDate = new DateTime(year, 1, 1);
+            while (currDate.Year == year)
+            {
+                int currDateDay = currDate.Day;
+                int currDateMonth = currDate.Month;
+                int sumOfdigits = digitsSum(currDateDay) + digitsSum(currDateMonth) + digitsSum(year);
+                if (sumOfdigits == sum)
+                {
+                    Console.WriteLine(currDate.ToString("dd MM yyyy = ") + sum.ToString());
+                }
+                currDate = currDate.AddDays(1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintDatesWithGivenSum(2000, 10);
+        }
+    }
+}
---
+++ Week 2/MethodsPartTwo/FridayThe13th/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridayThe13th
+{
+    class Program
+    {
+        public static int UnfortunateFridays(int startYear, int endYear)
+        {
+            int fridays = 0;
+            DateTime start = new DateTime(startYear,1,13);
+
+            while (start.Year <= endYear)
+            {
+                if (start.DayOfWeek == DayOfWeek.Friday)
+                {
+                    fridays++;
+                }
+                start = start.AddMonths(1);
+            }
+            return fridays;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UnfortunateFridays(1, 2011));
+        }
+    }
+}
