d803af1fe11757032fb815078e7d6764a59b5a3f
Viktor Barzin <vbarzin@gmail.com>
2015-12-10 20:19:16 +0200

    week 4 generics

---
+++ Week 4/GenericTypes/CreateGenericStackClass/Dequeue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateGenericStackClass
+{
+    class Dequeue<T>
+    {
+        private LinkedList<T> list = new LinkedList<T>();
+        public Dequeue()
+        {
+
+        }
+
+        public LinkedList<T> Clear()
+        {
+            this.list = new LinkedList<T>();
+            return list;
+        }
+
+        public bool Contains(T item)
+        {
+            if (list.Contains(item))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public LinkedList<T> RemoveFromFront()
+        {
+            list.RemoveFirst();
+            return list;
+        }
+
+        public LinkedList<T> RemoveFromEnd()
+        {
+            list.RemoveLast();
+            return list;
+        }
+
+        public LinkedList<T> AddToFront(T item)
+        {
+            list.AddFirst(item);
+            return list;
+        }
+
+        public LinkedList<T> AddToEnd(T item)
+        {
+            list.AddLast(item);
+            return list;
+        }
+
+        public T PeekFromFront()
+        {
+            return list.ToList()[list.ToList().Count - 1];
+        }
+
+        public T PeekFromEnd()
+        {
+            return list.ToList()[list.ToList().Count - 1];
+        }
+    }
+}
--- Week 4/GenericTypes/CreateGenericStackClass/Program.cs
+++ Week 4/GenericTypes/CreateGenericStackClass/Program.cs
@@ -10,13 +10,15 @@ namespace CreateGenericStackClass
     {
         static void Main(string[] args)
         {
-            List<object> listOne = new List<object>() { 3.20, 'v',1,"as" };
-            TotoGame toto1 = new TotoGame(listOne);
+            //List<object> listOne = new List<object>() { 3.20, 'v',1,"as" };
+            //TotoGame toto1 = new TotoGame(listOne);
 
-            List<object> listTwo = new List<object>() { 3.20, 'v',1 ,"ass"};
-            TotoGame toto2 = new TotoGame(listTwo);
+            //List<object> listTwo = new List<object>() { 3.20, 'v',1 ,"ass"};
+            //TotoGame toto2 = new TotoGame(listTwo);
 
-            Console.WriteLine(toto1 == toto2);
+            //Console.WriteLine(toto1 == toto2);
+            
+
         }
     }
 }
