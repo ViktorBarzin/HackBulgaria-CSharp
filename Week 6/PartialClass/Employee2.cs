04c4ad6113f8dae74344916b1c47a18f4b82ecb7
EdinPriqtel <vbarzin@gmail.com>
2016-01-18 11:36:25 +0200

    week 7

---
+++ Week 6/PartialClass/Employee2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    public partial class Employee
+    {
+        partial void Print()
+        {
+            Console.Write(string.Format("{0} {1}", this.FirstName, this.LastName));
+        }
+
+        public decimal CalculateAllIncome()
+        {
+            this.Print();
+            return this.Salary + this.Bonus;
+        }
+
+        public decimal CalculateBalance()
+        {
+            return (this.Salary + this.Bonus) * (decimal)0.8;
+        }
+    }
+}
---
+++ Week 6/PartialClass/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PartialClass
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Application
+    {
+        static void Main()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "goshko";
+            emp.LastName = "peshkov";
+            emp.Salary = 1000;
+            emp.Bonus = 200;
+            Console.WriteLine(emp.CalculateAllIncome());
+        }
+    }
+}
