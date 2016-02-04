125e139b86b0cfa7fe8e3e5f9ed69328e2c2487d
EdinPriqtel <vbarzin@gmail.com>
2016-02-04 22:04:26 +0200

    linq to sql

---
+++ Week 10/BooksWithLinqToSQL/HackLibraryApplication/DbConnection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackLibraryApplication
+{
+    using System.Data.SqlClient;
+
+    public class DbConnection : IDisposable
+    {
+        private HackLibraryDataContext context;
+
+        public DbConnection(HackLibraryDataContext context)
+        {
+            this.context = context;
+        }
+
+        public bool Insert(Book book)
+        {
+            try
+            {
+                this.context.Books.InsertOnSubmit(book);
+                this.context.SubmitChanges();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Insert(Author author)
+        {
+            try
+            {
+                this.context.Authors.InsertOnSubmit(author);
+                this.context.SubmitChanges();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+        }
+    }
+}
---
+++ Week 10/BooksWithLinqToSQL/HackLibraryApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackLibraryApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO : Get all books contained in the library
+            string input = Console.ReadLine();
+            Application(input);
+
+        }
+
+        private static void Application(string input)
+        {
+            do
+            {
+                string[] commands = input.ToLower().Split(' ');
+                switch (commands[0])
+                {
+                    case "insert":
+                        // Insert Book/Author
+                        break;
+                    case "get":
+                        // Get all books contained in the library - sorted in alphabetical order by title.
+                        // Get all books contained in the library sorted by author.
+                        // Get all books sorted by gendre.
+                        // Get all types of genders a given author has written.
+                        // Get all books written by a given author
+                        // Get the complete information of a book by its ISBN
+                        // Get the complete information by Title(part of the title should also be accepted)
+                        //
+                        break;
+                    case "loan":
+                        // loan books
+                        break;
+                    case "exit":
+                    case "quit":
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            while (true);
+        }
+    }
+}
