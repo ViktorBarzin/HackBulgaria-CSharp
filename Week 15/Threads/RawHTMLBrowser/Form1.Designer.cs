b2421fedf8358ace4cac21322e4760d33bc26165
EdinPriqtel <vbarzin@gmail.com>
2016-03-18 00:10:18 +0200

    week 13

---
+++ Week 15/Threads/RawHTMLBrowser/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace RawHTMLBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtOutput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtUrl
+            // 
+            this.TxtUrl.Location = new System.Drawing.Point(121, 43);
+            this.TxtUrl.Name = "TxtUrl";
+            this.TxtUrl.Size = new System.Drawing.Size(340, 20);
+            this.TxtUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            // 
+            // TxtOutput
+            // 
+            this.TxtOutput.Location = new System.Drawing.Point(29, 120);
+            this.TxtOutput.Multiline = true;
+            this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.Size = new System.Drawing.Size(449, 249);
+            this.TxtOutput.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(386, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 407);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtOutput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Week 15/Threads/RawHTMLBrowser/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RawHTMLBrowser
+{
+    using System.Net;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.button1.Text == "Load")
+            {
+                if (this.TxtUrl.Text == string.Empty)
+                {
+                    MessageBox.Show("Enter Url first!");
+                    return;
+                }
+                this.UpdateButtonTextToCancel();
+
+                string url = this.TxtUrl.Text;
+                this.DownloadHtmlThread(url);
+            }
+            else
+            {
+
+            }
+        }
+
+        private void DownloadHtmlThread(string url)
+        {
+            Thread thread = new Thread(
+                () =>
+                    {
+                        using (WebClient webClient = new WebClient())
+                        {
+                            lock (this)
+                            {
+                                string source = (webClient.DownloadString(url));
+                                this.Callback(source);
+                            }
+                        }
+                    }) { IsBackground = true };
+            thread.Start();
+            Thread.Sleep(2000);
+
+        }
+
+        private void Callback(string html)
+        {
+            this.TxtOutput.Text = html;
+            this.UpdateButtonTextToLoad();
+        }
+
+        private void UpdateButtonTextToCancel()
+        {
+            this.button1.Text = "Cancel";
+        }
+        private void UpdateButtonTextToLoad()
+        {
+            this.button1.Text = "Load";
+        }
+    }
+}
